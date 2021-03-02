1eb318f6999b5b7929d23802288eaa9f70f434d3
Ricky Thai <rickyvanthai@gmail.com>
2021-03-01 17:48:50 -0800

    Create App component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "styles/GlobalStyles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Components
+import Hero from "components/Hero";
+import Intro from "components/Intro";
+import IntroImage from "components/IntroImage";
+import Service from "components/Service";
+import Work from "components/Work";
+import WorkIcons from "components/WorkIcons";
+import Product from "components/Product";
+import Reviews from "components/Reviews";
+import Carousel from "components/Carousel";
+import Footer from "components/Footer";
+
+const App = () => (
+  <main>
+    <Hero />
+    <Intro />
+    <IntroImage />
+    <Service />
+    <Work />
+    <WorkIcons />
+    <Product />
+    <Reviews />
+    <Carousel />
+    <Footer />
+  </main>
+);
 
 export default App;
